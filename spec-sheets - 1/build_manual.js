c61207838e645c8acb1bb7dbadd47c4d2011be62
ekiepo <danny@ekiepo.com>
2026-07-08 11:08:30 -0500

    mobile floating button

--- spec-sheets - 1/build_manual.js
+++ spec-sheets - 1/build_manual.js
@@ -45,6 +45,13 @@ const head = `<!DOCTYPE html>
 
 const foot = `</main>
 
+<a href="#contents" class="floating-toc-btn" aria-label="Go to Table of Contents">
+  <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+    <line x1="12" y1="19" x2="12" y2="5"></line>
+    <polyline points="5 12 12 5 19 12"></polyline>
+  </svg>
+</a>
+
 <script src="assets/spec-sheet.js"></script>
 </body>
 </html>
